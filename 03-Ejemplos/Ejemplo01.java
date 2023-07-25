48086f0730242c1992ea31a3032e197386d46318
oaloaiza9 <oaloaiza9@misena.edu.co>
2023-07-25 13:23:21 -0500

    Carpeta base ADSO

---
+++ 03-Ejemplos/Ejemplo01.java
@@ -0,0 +1,31 @@
+
+public class Ejemplo01{
+	public static void main(String[] args) {
+		
+		// Instrucion de salida
+		byte edad = 18;
+		short turno = 938;
+		int total_vendido = 2500000;
+		long poblacion_mundial = 8000000000l;
+
+		float estatura = 1.85f;
+		double pi = 3.14159265;
+
+		boolean bandera = true;
+		char letra = 'X';
+
+		String saludo = "Hola Mundo";
+
+		System.out.println("Edad: "+edad);
+		System.out.println("Turno: "+turno);
+		System.out.println("Total Vendido: "+total_vendido);
+		System.out.println("Poblacion Mundial: "+poblacion_mundial);
+		System.out.println("Estatura: "+estatura);
+		System.out.println("PI: "+pi);
+		System.out.println("Bandera: "+bandera);
+		System.out.println("Letra: "+letra);
+		System.out.println("Saludo: "+saludo);		
+
+		
+	}
+}
