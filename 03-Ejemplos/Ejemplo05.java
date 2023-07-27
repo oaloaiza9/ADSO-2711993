4eda946ec2c41e4f279c02e3e7e3a84ca6d27c3d
oaloaiza9 <oaloaiza9@misena.edu.co>
2023-07-27 16:59:57 -0500

    Agregar gitignore

---
+++ 03-Ejemplos/Ejemplo05.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ejemplo05{
+	public static void main(String[] args) {
+		
+		// Crear variable de entrada
+		Scanner entrada = new Scanner(System.in);
+
+		
+
+	}
+}
