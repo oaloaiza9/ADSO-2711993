197f02c6b92957df643782d1d5013bb46a29b0a4
oaloaiza9 <oaloaiza9@misena.edu.co>
2023-10-10 14:49:22 -0500

    Primer parte de cajero electronico

--- 06 - Ejemplos POO/04 - Cajeros y Tarjetas/Principal.java
+++ 06 - Ejemplos POO/04 - Cajeros y Tarjetas/Principal.java
@@ -2,7 +2,14 @@
 public class Principal{
 	public static void main(String[] args) {
 		
+		CajeroElectronico cajero_01 = new CajeroElectronico(0, 10, 10, 10, 10, "Davivienda", "andres12345", "12345");
+		CajeroElectronico cajero_02 = new CajeroElectronico(300000000, 100, 100, 100, 100, "Bancolombia", "daniela54321", "12345");
 		
+		cajero_02.imprimirDetalleCajero();
+		cajero_02.abastecerCajero("daniela54321", "12345", 200, 200, 500, 500);
+		cajero_02.imprimirDetalleCajero();
+
 		
+
 	}
 }
